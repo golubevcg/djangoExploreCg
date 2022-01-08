78765fc5a483b79a4ecf401f1988f9f492397e00
Andrew Golubev <golubevcg@gmail.com>
2022-01-08 23:44:46 +0300

    in user removed overriden fields password, removed is_staff and is_deleted, all this fields coming from AbstractUser

--- explorecg/users/models.py
+++ explorecg/users/models.py
@@ -4,7 +4,6 @@ from django.db import models
 
 class ECGUser(AbstractUser):
     login = models.CharField(unique=True, null=False, max_length=50)
-    password = models.fields.CharField(max_length=50, null=False, unique=True)
     email = models.EmailField(unique=True, null=False)
 
     workplace = models.CharField(max_length=50)
@@ -12,9 +11,6 @@ class ECGUser(AbstractUser):
     experience = models.TextField()
 
     info = models.TextField()
-
-    is_deleted = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
 
     def __str__(self):
         return f"ECGUser({self.login}, {self.email})"
